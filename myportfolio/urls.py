bab1f2f3ca40e045a71957abbb8c1e01e7dfb9fd
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2025-04-21 12:30:44 +0000

    🔥🚀feat: config pour deploiement

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -19,12 +19,12 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.contrib.staticfiles import staticfiles_urlspaterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('work.urls')),
 ]
-urlpatterns +=staticfiles_urlspaterns()
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
