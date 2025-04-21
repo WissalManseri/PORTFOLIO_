3bc4d6ea2c55a4220d272292f2ab4fba4183397d
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2025-04-21 12:17:02 +0000

    feat: config pour deploiement

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u+(!8zt*93ein0&_0+8d0j^a9a(&#db9+1cd+!hwan)zn5udvp'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,14 +78,17 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 #pip install djongo
 
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-            }
+# "DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#             }
+# }
+# "
+import dj_database_url
+DATABASES ={
+    'default':dj_database_url.parse(config('DATABASE_URL'))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
